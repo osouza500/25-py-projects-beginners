a0379c4f7cfff152be285d59ca84de1bf7950d9d
osouza <mauricio.souza002@gmail.com>
2022-12-19 16:21:04 -0200

    Criar função printar_pausar

--- 01_madlibs/madlibs.py
+++ 01_madlibs/madlibs.py
@@ -1,26 +1,31 @@
 from art import text2art
 import random
+import time
+
+def printar_pausar(mensagem):
+    time.sleep(1)
+    print(mensagem)
 
 def continuar_sair():
     while True:
-        print("Jogar outra vez? (S/n)")
+        printar_pausar("Jogar outra vez? (S/n)")
         resposta = input().lower()
         print("")
         if resposta == "s":
             mad_libs()
         elif resposta == "n":
-            print("Até logo!")
+            printar_pausar("Até logo!")
             quit()
         else:
-            print("Input inválido.")
+            printar_pausar("Input inválido.")
 
 def sentenca_aleatoria(nome, subst):
     sentenca = random.choice([
         f"{nome} comeu um(a) {subst} estragad@ e passou mal.",
         f"{nome} foi à guerra e atirou um(a) {subst} em um prussiano.",
-        f"{nome} foi pres@ porque roubou um(a) {subst}.",
-        f"{nome} atropelou um(a) {subst} e fugiu sem prestar socorro."
-        f"{nome} tropeçou em um(a) {subst}, caiu e foi hospitalizad@.",
+        f"{nome} foi pres@ ao roubar um(a) {subst}.",
+        f"{nome} atropelou um(a) {subst}.",
+        f"{nome} tropeçou em um(a) {subst}.",
         f"{nome} foi ao pomar e colheu um(a) {subst}.",
         f"{nome} deu um(a) {subst} a um padre.",
         f"{nome} temperou o frango com {subst}.",
@@ -32,14 +37,17 @@ def sentenca_aleatoria(nome, subst):
 
 def mad_libs():
     print("Bem-vind@ a...\n")
-    print(text2art("Mad Libs!"))
-    print("Digite um nome próprio:")
+    printar_pausar(text2art("Mad Libs!"))
+    printar_pausar("Digite um nome próprio:")
     nome_proprio = input().capitalize()
     print("")
-    print("Agora, digite um substantivo no singular:")
+    printar_pausar("Pronto. Agora, digite um substantivo no singular:")
     substantivo = input().lower()
-    print("Gerando sentença...")
-    print(sentenca_aleatoria(nome_proprio, substantivo))
+    print("")
+    printar_pausar("Gerando sentença...")
+    print("")
+    printar_pausar(sentenca_aleatoria(nome_proprio, substantivo))
+    print("")
     continuar_sair()
 
 mad_libs()
