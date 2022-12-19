227295d02387d8e3800eaa602478324bafa4bb33
osouza <mauricio.souza002@gmail.com>
2022-12-19 10:13:52 -0200

    Criar lista de sentenças na função madlibs

--- 01_madlibs/madlibs.py
+++ 01_madlibs/madlibs.py
@@ -13,4 +13,23 @@ def continuar_sair():
             print("Input inválido.")
 
 def mad_libs():
+    sentencas = [
+        [f"{nome_proprio} acordou de madrugada e foi até "
+         f"a cozinha. Abriu a geladeira e viu {substantivo}. "
+         f"Pegou, comeu e {verbo_passado}."],
+        [f"{nome_proprio} foi à guerra. Encontrou um baú "]
+    
+    ]
+
+    print("Bem-vind@ a...\n")
     print(text2art("Mad Libs!"))
+    print("Digite um nome próprio:\n")
+    nome_proprio = input().lower()
+    print("Agora, digite um substantivo:\n")
+    substantivo = input().lower()
+    print("Por fim, digite um verbo conjugado no passado:\n")
+    verbo_passado = input().lower()
+    print("...")
+
+
+mad_libs()
