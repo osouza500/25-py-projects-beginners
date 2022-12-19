e4873835c512db410e7342d874cf305b4649144b
osouza <mauricio.souza002@gmail.com>
2022-12-19 11:08:09 -0200

    Atualizar lista de sentenças e variáveis de input

--- 01_madlibs/madlibs.py
+++ 01_madlibs/madlibs.py
@@ -1,8 +1,9 @@
 from art import text2art
+import random
 
 def continuar_sair():
     while True:
-        print("Jogar outra vez? (S/n)\n")
+        print("Jogar outra vez? (S/n)")
         resposta = input().lower()
         if resposta == "s":
             mad_libs()
@@ -13,23 +14,31 @@ def continuar_sair():
             print("Input inválido.")
 
 def mad_libs():
-    sentencas = [
-        [f"{nome_proprio} acordou de madrugada e foi até "
-         f"a cozinha. Abriu a geladeira e viu {substantivo}. "
-         f"Pegou, comeu e {verbo_passado}."],
-        [f"{nome_proprio} foi à guerra. Encontrou um baú "]
-    
-    ]
-
     print("Bem-vind@ a...\n")
     print(text2art("Mad Libs!"))
-    print("Digite um nome próprio:\n")
-    nome_proprio = input().lower()
-    print("Agora, digite um substantivo:\n")
+    print("Digite um nome próprio:")
+    nome_proprio = input()
+    print("")
+    print("Agora, digite um substantivo no singular:")
     substantivo = input().lower()
-    print("Por fim, digite um verbo conjugado no passado:\n")
-    verbo_passado = input().lower()
-    print("...")
+    print("Gerando sentença...")
+    sentencas = [
+        f"{nome_proprio} comeu um(a) {substantivo} estragad@, "
+         "passou mal e desmaiou.",
+        f"{nome_proprio} foi à guerra e atirou um(a) {substantivo} "
+         "em um soldado prussiano.",
+        f"{nome_proprio} foi pres@ porque roubou um(a) {substantivo}.",
+        f"{nome_proprio} atropelou um(a) {substantivo} e fugiu sem "
+         "prestar socorro.",
+        f"Desastrad@, {nome_proprio} tropeçou em um(a) {substantivo}, "
+         "caiu e foi hospitalizad@.",
+        f"Sem motivo para tal, {nome_proprio} amarrou um(a) {substantivo} "
+         "em uma árvore e ficou ali observando.",
+        f"O cachorro de {nome_proprio} comeu um {substantivo} e enlouqueceu."
+        f"O gato de {nome_proprio} adora arranhar {substantivo}."
+    ]
+    print(random.choice(sentencas))
+    continuar_sair()
 
 
 mad_libs()
