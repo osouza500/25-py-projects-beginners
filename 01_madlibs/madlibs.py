526c4980685f3430043a96104805e15ed31a944c
osouza <mauricio.souza002@gmail.com>
2022-12-19 10:01:06 -0200

    Criar função continuar_sair

--- 01_madlibs/madlibs.py
+++ 01_madlibs/madlibs.py
@@ -1,3 +1,16 @@
 from art import text2art
-print(text2art("Mad Libs!"))
 
+def continuar_sair():
+    while True:
+        print("Jogar outra vez? (S/n)\n")
+        resposta = input().lower()
+        if resposta == "s":
+            mad_libs()
+        elif resposta == "n":
+            print("Até logo!")
+            quit()
+        else:
+            print("Input inválido.")
+
+def mad_libs():
+    print(text2art("Mad Libs!"))
