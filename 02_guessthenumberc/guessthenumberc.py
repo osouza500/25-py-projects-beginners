196bdbb7c8c17bf995aeda33a4792d93f2b8a07e
osouza <mauricio.souza002@gmail.com>
2022-12-20 11:00:45 -0200

    Inserir try/except no loop while da função adivinhe_numero

--- 02_guessthenumberc/guessthenumberc.py
+++ 02_guessthenumberc/guessthenumberc.py
@@ -3,7 +3,7 @@ import time
 
 
 def printar_pausar(mensagem):
-    time.sleep(1)
+    time.sleep(.5)
     print(mensagem)
 
 
@@ -12,7 +12,7 @@ def continuar_sair():
         printar_pausar("Jogar novamente? (S/n)")
         resposta = input().lower()
         if resposta == "s":
-            adivinhe_numero()
+            adivinhe_numero(10)
         elif resposta == "n":
             printar_pausar("Até logo!")
             break
@@ -23,16 +23,21 @@ def continuar_sair():
 def adivinhe_numero(numero):
     numero_maquina = random.randint(1, numero)
     numero_usuario = 0
+    printar_pausar("Bem-vind@ a...\n")
+    printar_pausar("Adivinhe o Número!!!\n")
     while numero_usuario != numero_maquina:
-        printar_pausar(f"Digite um número entre 1 e {numero}")
-        numero_usuario = int(input())
-        if numero_usuario > numero_maquina:
-            printar_pausar("Chutou alto. Tente novamente.")
-        elif numero_usuario < numero_maquina:
-            printar_pausar("Chutou baixo. Tente novamente.")
-        else:
-            printar_pausar(f"Acertou! o número escolhido pela "
-                           f"máquina foi {numero_maquina}")
-            continuar_sair()
+        try:
+            printar_pausar(f"Digite um número entre 1 e {numero}")
+            numero_usuario = int(input())
+            print("")
+            if numero_usuario > numero_maquina:
+                printar_pausar("Chutou alto. Tente novamente.\n")
+            elif numero_usuario < numero_maquina:
+                printar_pausar("Chutou baixo. Tente novamente.\n")
+        except ValueError:
+            printar_pausar("Input inválido.\n")        
+    printar_pausar(f"Acertou! o número escolhido pela "
+                   f"máquina foi {numero_maquina}.\n")
+    continuar_sair()
 
 adivinhe_numero(10)
