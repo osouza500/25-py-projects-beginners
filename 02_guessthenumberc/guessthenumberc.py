40c404729bab9cf4460a5248f72a4d7686fe8cec
osouza <mauricio.souza002@gmail.com>
2022-12-20 10:25:42 -0200

    Criar função adivinhe_numero

--- 02_guessthenumberc/guessthenumberc.py
+++ 02_guessthenumberc/guessthenumberc.py
@@ -6,6 +6,7 @@ def printar_pausar(mensagem):
     time.sleep(1)
     print(mensagem)
 
+
 def continuar_sair():
     while True:
         printar_pausar("Jogar novamente? (S/n)")
@@ -17,3 +18,21 @@ def continuar_sair():
             break
         else:
             printar_pausar("Input inválido.")
+
+
+def adivinhe_numero(numero):
+    numero_maquina = random.randint(1, numero)
+    numero_usuario = 0
+    while numero_usuario != numero_maquina:
+        printar_pausar(f"Digite um número entre 1 e {numero}")
+        numero_usuario = int(input())
+        if numero_usuario > numero_maquina:
+            printar_pausar("Chutou alto. Tente novamente.")
+        elif numero_usuario < numero_maquina:
+            printar_pausar("Chutou baixo. Tente novamente.")
+        else:
+            printar_pausar(f"Acertou! o número escolhido pela "
+                           f"máquina foi {numero_maquina}")
+            continuar_sair()
+
+adivinhe_numero(10)
