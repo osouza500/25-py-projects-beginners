9d071d147c1e949fbfb8a64d5f04ea4b340378ae
osouza <mauricio.souza002@gmail.com>
2022-12-21 16:23:05 -0200

    Inserir loop while e instrução condicional na função ai_computador

--- 03_guessthenumberu/guessthenumberu.py
+++ 03_guessthenumberu/guessthenumberu.py
@@ -18,12 +18,30 @@ def continuar_sair():
         else:
             printar_pausar("Input inválido.")
 
-def adivinhe_numero():
-    printar_pausar("Digite um número de 1 a 10; "
-                   "o computador tentará adivinhá-lo.")
-    resposta = int(input())
-    return resposta
+def input_usuario():
+    while True:
+        printar_pausar("Escolha um número de 1 a 10. "
+                    "O computador tentará adivinhá-lo.")
+        resposta = int(input())
+        if resposta <= 0 or resposta > 10:
+            printar_pausar("Input inválido.")
+        ai_computador(resposta)
 
 
 def ai_computador(numero):
-    
+    tentativas = []
+    while True:
+        jogada_computador = random.randint(1, 10)
+        tentativas.append(jogada_computador)       
+        printar_pausar(f"O número é {jogada_computador}?")
+        if jogada_computador < numero:
+            printar_pausar( "Acho que chutei baixo... "
+                            "Vou tentar mais uma vez.")
+        elif jogada_computador > numero:
+            printar_pausar("Acho que chutei alto... "
+                            "Vou tentar mais uma vez.")
+        else:
+            printar_pausar(f"Acertei! O número que você escolheu é {numero}!")
+            continuar_sair()
+
+input_usuario()
